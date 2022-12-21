f570a62754158694443bbee7928e1c848b2c70d7
Lauma Cirule <laumii@gmail.com>
2022-12-21 09:16:28 +0200

    Integrated new and create actions for portfolios

--- app/controllers/portfolios_controller.rb
+++ app/controllers/portfolios_controller.rb
@@ -3,4 +3,26 @@ class PortfoliosController < ApplicationController
     @portfolio_items = Portfolio.all
   end
 
+  def new
+    @portfolio_item = Portfolio.new
+  end
+
+  def create
+    @portfolio_item = Portfolio.new(portfolio_item_params)
+
+    respond_to do |format|
+      if @portfolio_item.save
+        format.html { redirect_to portfolios_path, notice: "Your portfolio item is now live!" }
+      else
+        format.html { render :new, status: :unprocessable_entity }
+      end
+    end
+  end
+
+  private
+
+  # Only allow a list of trusted parameters through.
+    def portfolio_item_params
+      params.require(:portfolio).permit(:title, :subtitle, :body)
+    end
 end
